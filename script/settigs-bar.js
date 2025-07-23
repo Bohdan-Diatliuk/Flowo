27033a3e9e3b4de3085cf8c5339da38ba727b5ad
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-07-23 21:21:11 +0300

    was added Settings page, but not all

---
+++ script/settigs-bar.js
@@ -0,0 +1,6 @@
+ const titleSet = document.getElementById('title-set');
+  const settings = document.getElementById('settings-container');
+
+  titleSet.addEventListener('click', () => {
+    settings.classList.toggle('hidden');
+  })
